c85cc0e97de6e618840afdc24742e50aaed584b3
oldnick85 <tyapin_nikita@mail.ru>
2022-11-27 20:58:25 +0300

    [add] bump to version 2.44.0

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build \
     --build-arg UBUNTU_VERSION=22.04 \
-    --build-arg I2PD_VERSION=2.43.0 \
+    --build-arg I2PD_VERSION=2.44.0 \
     --build-arg I2PD_COMPILER=gcc \
-    --tag i2pd:2.43.0 \
+    --tag i2pd:2.44.0 \
     .
